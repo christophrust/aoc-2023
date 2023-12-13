70743d44347e62c0495820e0a1363ede92d97b1d
Christoph Rust <christoph.g.rust@gmail.com>
2023-12-13 20:35:37 +0100

    tmp: wip

--- d12/src/main.rs
+++ d12/src/main.rs
@@ -1,8 +1,8 @@
-use rayon::prelude::{IntoParallelIterator, ParallelIterator};
+// use rayon::prelude::{IntoParallelIterator, ParallelIterator};
 use regex::Regex;
 use std::fs::File;
 use std::io::{self, BufRead};
-
+use cached::proc_macro::cached;
 
 fn main() {
     let re = Regex::new(r"[0-9]+").unwrap();
@@ -15,7 +15,7 @@ fn main() {
             let x = x.unwrap();
             let d = re.find_iter(&x).map(|x| x.as_str().parse().unwrap()).collect();
             let s = x.split(' ').next().unwrap();
-            permute(s, &d)
+            permute(s.to_string(), d)
         })
         .sum();
 
@@ -24,10 +24,12 @@ fn main() {
     let file = File::open("input.txt").unwrap();
     let res: i64 = io::BufReader::new(&file)
         .lines()
-        .map(Result::unwrap)
-        .collect::<Vec<String>>()
-        .into_par_iter()
-        .fold(|| 0_i64, |a, x| {
+        // .map(Result::unwrap)
+        // .collect::<Vec<String>>()
+        // .into_par_iter()
+        // .fold(|| 0_i64, |a, x| {
+        .map(|x|{
+            let x = x.unwrap();
             println!("{}", x);
             let d: Vec<i64> = re.find_iter(&x).map(|x| x.as_str().parse().unwrap()).collect();
             let s = x.split(' ').next().unwrap().to_string();
@@ -40,35 +42,37 @@ fn main() {
                               a.push_str(&i);
                               a
                           });
-            a + permute(&s, &d)
+            // a + permute(&s, &d)
+            // a + dc_permute(&s, &d)
+            permute(s, d)
         })
         .sum::<i64>();
 
     println!("Part 2: {}", res);
 }
 
-fn factorial(x: i64) -> i64 {
-    if x == 0 || x == 1 {
-        return 1;
-    }
-    (1..=x).into_iter().product()
-}
 
-fn binom(n: i64, k: i64) -> i64 {
-    if k > n {
-        return 0;
-    }
-    factorial(n) / (factorial(k) * factorial(n-k))
-}
-
-fn permute(s: &str, r: &Vec<i64>) -> i64 {
+#[cached]
+fn permute(s: String, r: Vec<i64>) -> i64 {
 
     // println!("called with {} and {:?}", s, r);
     let s = s.trim_matches('.');
 
 
+    if r.len() == 0 {
+        return match s.find('#') {
+            None => 1,
+            Some(_) => 0,
+        }
+    }
 
+
+    let mut res = 0;
     if s.len() < (r.iter().sum::<i64>() as usize + r.len() - 1) || s.len() == 0 {
+        return 0;
+    }
+
+    if s.chars().filter(|&x| x == '#').count() > r.iter().sum::<i64>() as usize {
         return 0;
     }
 
@@ -84,6 +88,8 @@ fn permute(s: &str, r: &Vec<i64>) -> i64 {
         return rr;
     }
 
+
+
     let mut cnt = 0;
 
     let (p, remainder) = s.split_at(r[0] as usize);
@@ -95,7 +101,7 @@ fn permute(s: &str, r: &Vec<i64>) -> i64 {
         } else if r.len() > 1 {
             let (p, rem) = remainder.split_at(1);
             if p != "#" {
-                cnt += permute(rem, &r[1..].to_vec());
+                cnt += permute(rem.to_string(), r[1..].to_vec());
             }
         }
     }
@@ -103,7 +109,7 @@ fn permute(s: &str, r: &Vec<i64>) -> i64 {
     match s.split_at(1) {
         ("#", _) => {},
         (_, rem) => {
-            cnt += permute(rem, r);
+            cnt += permute(rem.to_string(), r);
         }
     }
 
@@ -112,30 +118,33 @@ fn permute(s: &str, r: &Vec<i64>) -> i64 {
 }
 
 
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_permute() {
-        assert_eq!(permute(".??..??...?##.", &vec![1,1,3]), 4);
-        assert_eq!(permute("????.######..#####.", &vec![1,6,5]), 4);
-        assert_eq!(permute("???#?##???", &vec![4,4]), 1);
-        assert_eq!(permute("????.######..#####.", &vec![1,6,5]), 4);
+        assert_eq!(permute(".??..??...?##.".to_string(), vec![1,1,3]), 4);
+        assert_eq!(permute("????.######..#####.".to_string(), vec![1,6,5]), 4);
+        assert_eq!(permute("???#?##???".to_string(), vec![4,4]), 1);
+        assert_eq!(permute("????.######..#####.".to_string(), vec![1,6,5]), 4);
 
-        assert_eq!(permute("?????", &vec![2,1]), 3);
-        assert_eq!(permute("?????", &vec![2,1]), 3);
-        assert_eq!(permute("?????", &vec![2]), 4);
+        assert_eq!(permute("?????".to_string(), vec![2,1]), 3);
+        assert_eq!(permute("?????".to_string(), vec![2,1]), 3);
+        assert_eq!(permute("?????".to_string(), vec![2]), 4);
 
-        assert_eq!(permute(".??..??...?##.?.??..??...?##.?.??..??...?##.?.??..??...?##.?.??..??...?##.",
-                             &vec![1,1,3,1,1,3,1,1,3,1,1,3,1,1,3]), 16384);
+        assert_eq!(permute(".??..??...?##.?.??..??...?##.?.??..??...?##.?.??..??...?##.?.??..??...?##.".to_string(),
+                             vec![1,1,3,1,1,3,1,1,3,1,1,3,1,1,3]), 16384);
+
         assert_eq!(
-            permute("???.###????.###????.###????.###????.###",
-            &vec![1,1,3,1,1,3,1,1,3,1,1,3,1,1,3]), 1
+            permute("???.###????.###????.###????.###????.###".to_string(),
+            vec![1,1,3,1,1,3,1,1,3,1,1,3,1,1,3]), 1
         );
         assert_eq!(
-            permute("?###??????????###??????????###??????????###??????????###????????",
-            &vec![3,2,1,3,2,1,3,2,1,3,2,1,3,2,1]), 506250
+            permute("?###??????????###??????????###??????????###??????????###????????".to_string(),
+            vec![3,2,1,3,2,1,3,2,1,3,2,1,3,2,1]), 506250
         );
 
     }
@@ -145,4 +154,5 @@ mod tests {
         assert_eq!(binom(3,2), 3);
         assert_eq!(binom(4,2), 6);
     }
+
 }
