a75e59e1cbf8470fb83acd9d85671713fd68ac9e
Christoph Rust <christoph.g.rust@gmail.com>
2023-12-15 17:02:22 +0100

    make cargo clippy and cargo fmt happy

--- d15/src/main.rs
+++ d15/src/main.rs
@@ -1,8 +1,6 @@
 use std::collections::HashMap;
 
-
 fn main() {
-
     let res = include_bytes!("../input.txt")
         .split(|&x| x == b',')
         .map(|x| hash(x) as u32)
@@ -14,7 +12,6 @@ fn main() {
         boxes.push(LenseBox::new());
     }
 
-
     include_bytes!("../input.txt")
         .split(|&x| x == b',')
         .for_each(|x| {
@@ -24,49 +21,52 @@ fn main() {
             let b = &mut boxes.get_mut(hash(l) as usize).unwrap().slots;
 
             match d.next() {
-                Some([10]) => {},
+                Some([10]) => {} // ignore new line character at end of file
                 Some([foc]) => {
                     if let Some(lens) = b.get_mut(&label) {
                         lens.0 = *foc - 48;
                     } else {
                         b.insert(label, (*foc - 48, b.len()));
                     }
-                },
+                }
                 Some([]) => {
                     let pos = match b.get(&label) {
-                        Some((_,p)) => Some(*p),
+                        Some((_, p)) => Some(*p),
                         _ => None,
                     };
                     if let Some(pos) = pos {
                         b.remove(&label);
-                        b.iter_mut().for_each(|(_,(_,p))| {
+                        b.iter_mut().for_each(|(_, (_, p))| {
                             if *p > pos {
                                 *p -= 1;
                             }
                         })
                     }
-                },
+                }
                 None => {
                     unreachable!("{}, {:?}", std::str::from_utf8(x).unwrap(), x);
-                },
+                }
                 Some(x) => {
                     println!("{}, {:?}", std::str::from_utf8(x).unwrap(), x);
                     unreachable!()
-                },
+                }
             }
         });
 
     let mut cnt = 0;
-    let res = boxes.into_iter().map(|b| {
-        cnt += 1;
-        if b.slots.len() > 0 {
-            cnt * b.slots.iter().fold(0, |a,(_, (f,p))| {
-                a + *f as u32 * (*p as u32 + 1)
-            })
-        } else {
-            0
-        }
-    })
+    let res = boxes
+        .into_iter()
+        .map(|b| {
+            cnt += 1;
+            if !b.slots.is_empty() {
+                cnt * b
+                    .slots
+                    .iter()
+                    .fold(0, |a, (_, (f, p))| a + *f as u32 * (*p as u32 + 1))
+            } else {
+                0
+            }
+        })
         .sum::<u32>();
 
     println!("Part 2: {res}");
@@ -74,24 +74,25 @@ fn main() {
 
 #[allow(arithmetic_overflow)]
 fn hash(x: &[u8]) -> u8 {
-    x.into_iter().filter(|&&x| x != 10).fold(0, |a, i| {
-        let s = a.wrapping_add(*i);
-        (s << 4).wrapping_add(s)
-
-    })
+    x.iter()
+        .filter(|&&x| x != 10) // ignore new line character at end of file
+        .fold(0, |a, i| {
+            let s = a.wrapping_add(*i);
+            (s << 4).wrapping_add(s)
+        })
 }
-
 
 #[derive(Clone, Debug)]
 struct LenseBox {
-    slots: HashMap<Vec<u8>, (u8,usize)>
+    slots: HashMap<Vec<u8>, (u8, usize)>,
 }
 
 impl LenseBox {
     fn new() -> Self {
-        Self { slots: HashMap::<Vec<u8>, (u8,usize)>::new() }
+        Self {
+            slots: HashMap::<Vec<u8>, (u8, usize)>::new(),
+        }
     }
 }
-
 
 // impl Copy for LensBox {}
