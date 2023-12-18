71c610bb3fe7d74fb62b74f0b42e3db18e02b6de
Christoph Rust <christoph.g.rust@gmail.com>
2023-12-18 21:31:22 +0100

    wip

--- d18/src/main.rs
+++ d18/src/main.rs
@@ -5,12 +5,12 @@ use std::collections::HashSet;
 fn main() {
 
     let mut hm = HashSet::<(i64, i64)>::new();
-    let file = File::open("input.txt").unwrap();
     let mut rmin = 0;
     let mut cmin = 0;
     let mut rmax = 0;
     let mut cmax = 0;
 
+    let file = File::open("input.txt").unwrap();
     let poly = io::BufReader::new(&file)
         .lines()
         .map(|x| {
@@ -48,6 +48,58 @@ fn main() {
 
     let res = hm.len();
     println!("Part 1: {}", res);
+
+
+    let file = File::open("input.txt").unwrap();
+    let res = io::BufReader::new(&file)
+        .lines()
+        .map(|x| {
+            let x = x.unwrap();
+            let mut x = x.split(' ');
+            let d = match x.next().unwrap() {
+                "R" => (1_i64,0_i64),
+                "L" => (-1,0),
+                "U" => (0,-1),
+                "D" => (0,1),
+                _ => unreachable!(),
+            };
+            let t = x.next().unwrap().parse::<i64>().unwrap();
+            (d,t + 1)
+        })
+        .fold((0.0,(0_i64,0_i64)), |(a, (x1,y1)), ((d1,d2),t)| {
+            // println!("{}-{}, {}", d1, d2, t);
+            let x2 = x1 + t * d1;
+            let y2 = y1 + t * d2;
+            // println!("{},{}", x2, y2);
+            (a + 0.5 * (y1 + y2) as f64 * (x1 - x2) as f64, (x2, y2))
+        }).0;
+     println!("Part 1a: {}", res);
+
+
+    let re = regex::Regex::new(r"#([a-e0-9]{5})([0-3])").unwrap();
+
+    let file = File::open("input.txt").unwrap();
+    let res = io::BufReader::new(&file)
+        .lines()
+        .map(|x| {
+            let x = x.unwrap();
+            let (_,[t,d]) = re.captures(&x).map(|x| x.extract()).unwrap();
+            let t = t.chars().rev().zip(0..5).fold(0, |a,(c, p)| a + c.to_digit(16).unwrap() as i64 * 16_i64.pow(p)) + 1;
+            let d =  match d {
+                "0" => (0,1),
+                "2" => (0,-1),
+                "1" => (1,0),
+                "3" => (-1,0),
+                _ => unreachable!(),
+            };
+            (d,t)
+        })
+        .fold((0.0,(0,0)), |(a, (x1,y1)), ((d1,d2),t)| {
+            let x2 = x1 + t* d1;
+            let y2 = y1 + t* d2;
+            (a + 0.5 * (y1 + y2) as f64 * (x1 - x2) as f64, (x1, x2))
+        }).0;
+    println!("Part 2: {}", res);
 }
 
 
