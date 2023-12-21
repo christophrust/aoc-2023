7e1fad02ea746f6c4f3304e70c6eb188046b32ae
Christoph Rust <christoph.g.rust@gmail.com>
2023-12-21 21:17:20 +0100

    wip

--- d21/src/main.rs
+++ d21/src/main.rs
@@ -17,7 +17,7 @@ fn main() {
         .for_each(|x| {
             let x = x.unwrap();
             x.char_indices().for_each(|(c,cc)| {
-                nc = (c as isize).max(nc);
+                nc = (c as isize +1).max(nc);
                 match cc {
                     '#' => {stones.insert((r,c as isize));},
                     'S' => {spos = (r,c as isize);},
@@ -28,11 +28,15 @@ fn main() {
         });
     let nr = r;
 
+
+
+
     let mut pos = HashSet::<(isize,isize)>::new();
+    // pos.insert((0,129));
     pos.insert(spos);
 
-    println!("{}", pos.len());
-    for _i in 0..64 {
+
+    for _i in 0..146 {
         pos = pos
             .into_iter()
             .fold(HashSet::<(isize,isize)>::new(), |mut a,(r,c)| {
@@ -49,6 +53,48 @@ fn main() {
             });
     }
 
+
+
     let res = pos.len();
     println!("Part1: {}",res);
+
+
+    // for r in 0..nr {
+    //     let ln = (0..nc)
+    //         .into_iter()
+    //         .map(|c| {
+    //             match pos.get(&(r,c)) {
+    //                 Some(_) => 'O',
+    //                 None => '.',
+    //             }
+    //         })
+    //         .collect::<String>();
+    //     println!("{ln}");
+    // }
+    let mut pos = HashSet::<(isize,isize)>::new();
+    // pos.insert((0,129));
+    pos.insert(spos);
+
+    for _i in 0..(65 + 3 * 131) {
+        pos = pos
+            .into_iter()
+            .fold(HashSet::<(isize,isize)>::new(), |mut a,(r,c)| {
+                for (dr,dc) in [(1,0),(-1,0),(0,1), (0,-1)] {
+                    let np = (r + dr, c + dc);
+                    if stones.get(&(np.0 % 131, np.1 % 131)).is_none() {
+                        a.insert(np);
+                    }
+                }
+                a
+            });
+    }
+
+
+
+    let res = pos.len();
+    println!("Part2: {}",res);
+
+    // println!("nc: {}",nc);
+    // println!("nr: {}",nr);
+    // println!("spos: {:?}",spos);
 }
