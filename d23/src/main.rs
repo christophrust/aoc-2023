8b901a3af5f1280cf939aca054091018870fd931
Christoph Rust <christoph.g.rust@gmail.com>
2023-12-23 09:27:13 +0100

    add solution part 1 d23

--- d23/src/main.rs
+++ d23/src/main.rs
@@ -2,28 +2,43 @@
 
 fn main() {
 
-    let grid = include_bytes!("../input1.txt")
+    let grid = include_bytes!("../input.txt")
         .split(|x| *x == b'\n')
         .map(|x| x.to_vec())
+        .filter(|x| x.len() > 0)
         .collect::<Vec<Vec<u8>>>();
+
     let start_col =grid[0].iter().position(|x| *x == b'.').unwrap();
     let start =vec![ (0,start_col), (1,start_col)];
-    let ways = walk(&grid, start).unwrap();
+    let ways = walk(&grid, start, true).unwrap();
 
-    let res = ways.into_iter().fold(0, |a,i| {
+    println!("{:?}", ways);
+    let res = ways.iter().fold(0, |a,&i| {
         if a > i {
             return a;
         }
         i
     });
     println!("Part 1: {res}.");
+
+    let start =vec![ (0,start_col), (1,start_col)];
+    let ways = walk(&grid, start, false).unwrap();
+    let res = ways.iter().fold(0, |a,&i| {
+        if a > i {
+            return a;
+        }
+        i
+    });
+    println!("{:?}", ways);
+    println!("Part 2: {res}");
 }
 
 
-fn walk(grd: &Vec<Vec<u8>>, mut p_history: Vec<(usize,usize)>) -> Option<Vec<usize>> {
+fn walk(grd: &Vec<Vec<u8>>, mut p_history: Vec<(usize,usize)>, part1: bool) -> Option<Vec<usize>> {
 
     let (mut r, mut c) = p_history.last().unwrap();
     loop {
+        // println!("{r},{c}");
         let next : Vec<(usize, usize)>= [(1_i32,0_i32), (-1,0), (0,1), (0,-1)]
             .into_iter()
             .filter(|(dr,dc)| {
@@ -33,13 +48,14 @@ fn walk(grd: &Vec<Vec<u8>>, mut p_history: Vec<(usize,usize)>) -> Option<Vec<usi
                     return false
                 }
                 let pos = grd[rn][cn];
-                match (pos, *dr, *dc) {
-                    (b'#',_,_) => false,
-                    (b'.',_,_) => true,
-                    (b'>',_,1) => true,
-                    (b'<',_,-1) => true,
-                    (b'^',-1,_) => true,
-                    (b'v',1,_) => true,
+                match (pos, *dr, *dc, part1) {
+                    (b'#',_,_,_) => false,
+                    (b'.',_,_,_) => true,
+                    (b'>',_,1, true) => true,
+                    (b'<',_,-1, true) => true,
+                    (b'^',-1,_, true) => true,
+                    (b'v',1,_, true) => true,
+                    (_,_,_, false) => true,
                     _ => false,
                 }
             })
@@ -53,11 +69,12 @@ fn walk(grd: &Vec<Vec<u8>>, mut p_history: Vec<(usize,usize)>) -> Option<Vec<usi
             return None;
         }
         if next.len() == 1 {
+            // println!("{r},{c}");
             // maybe we have to add 1 to the result here?
+            (r,c) = (next[0].0,next[0].1);
             if r == grd.len() - 1 {
                 return Some(vec![p_history.len()])
             }
-            (r,c) = (next[0].0,next[0].1);
             p_history.push((r,c));
             continue;
         }
@@ -67,7 +84,7 @@ fn walk(grd: &Vec<Vec<u8>>, mut p_history: Vec<(usize,usize)>) -> Option<Vec<usi
                 .filter_map(|next| {
                     let mut p_hist = p_history.clone();
                     p_hist.push(next);
-                    walk(grd, p_hist)
+                    walk(grd, p_hist, part1)
                 })
                 .fold(vec![], |mut a,mut i| {
                     a.append(&mut i);
@@ -76,3 +93,4 @@ fn walk(grd: &Vec<Vec<u8>>, mut p_history: Vec<(usize,usize)>) -> Option<Vec<usi
         )
     }
 }
+
